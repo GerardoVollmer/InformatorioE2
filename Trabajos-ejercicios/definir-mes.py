33be3c64bf26e5ed7f9950f899cf80369a53651c
GerardoVollmer <@geravollmer17@gmail.com>
2025-05-05 18:28:26 -0300

    Aniado trabajo practico

---
+++ Trabajos-ejercicios/definir-mes.py
@@ -0,0 +1,10 @@
+# EJERCICIO: Tenemos que ingresar por pantalla un string con la fecha en formato ISO por ejemplo:
+# '20250505', identificar el mes en elque se encuentra en este caso .
+
+# SOLUCIÓN:
+
+
+#Trabajo 2 Ejercicio: Definir una lista de 1..1000 y determinen todos los numeros primos que aparecen
+# entre el 1 y el 1000, y guardarlos en una lista llamada numeros_primos.
+
+# SOLUCIÓN:
