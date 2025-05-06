de4cbcd8b1aac7ee3e0f9a01b49ba2e54e3b98ef
GerardoVollmer <@geravollmer17@gmail.com>
2025-05-06 16:27:53 -0300

    aprendiendo usar git

--- Trabajos-ejercicios/definir-mes.py
+++ Trabajos-ejercicios/definir-mes.py
@@ -7,4 +7,4 @@
 #Trabajo 2 Ejercicio: Definir una lista de 1..1000 y determinen todos los numeros primos que aparecen
 # entre el 1 y el 1000, y guardarlos en una lista llamada numeros_primos.
 
-# SOLUCIÓN:
+# SOLUCIÓN: 
