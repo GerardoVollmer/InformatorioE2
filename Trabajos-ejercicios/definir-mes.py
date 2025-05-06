a6799e8ce468dc1f38dc3e8b05caa16b37750448
GerardoVollmer <@geravollmer17@gmail.com>
2025-05-06 16:48:33 -0300

    resuelvo lista de numeros primos

--- Trabajos-ejercicios/definir-mes.py
+++ Trabajos-ejercicios/definir-mes.py
@@ -8,3 +8,15 @@
 # entre el 1 y el 1000, y guardarlos en una lista llamada numeros_primos.
 
 # SOLUCIÓN:
+# Creo la funcion para veriicar si un numero es primo
+def es_primo(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+#genero la lista de numeros primos del 1 al 1000
+numeros_primos = [num for num in range(1, 1001) if es_primo(num)]
+print(numeros_primos) #imprimo la lista de numeros primos
