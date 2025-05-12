8d810d7b5ee54fd8f9dc2dd0f949a5493a01ba73
GerardoVollmer <@geravollmer17@gmail.com>
2025-05-12 17:54:16 -0300

    Trabajamos con tuplas

---
+++ 20250512/lista.py
@@ -0,0 +1,21 @@
+#lista_1 = ['string', 123, False]
+
+#lista_2 = ['Ramon', 'Sere','Gerardo']
+
+#lista_3 = ['un dato', lista_1, 'Datos2', lista_2]
+# print(lista_3)
+# print(lista_3[1][0]) #imprime el primer elemento de la lista 1
+# lista_1.appende('valor nuevo') #agrega un nuevo elemento a la lista 1
+
+
+
+
+
+
+#lista = [1, 2, 3, 4, 5]
+# print(lista[0]) #imprime el primer elemento de la lista
+#lista.reverse() #invierte la lista
+#print(lista) #imprime la lista invertida
+
+tupla = (1, 2, 3, 4, 5) #tupla
+print(tupla[0]) #imprime el primer elemento de la tupla
